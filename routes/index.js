42e01725027654453811add1f499bbc08772d488
Sang Min Na <xsangmin@gmail.com>
2016-11-01 01:59:25 -0400

    delete unnecessary routes for /login and /signup

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,5 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/login', (req, res) => {
-  res.render('login');
-});
-
-router.get('/signup', (req, res) => {
-  res.render('signup');
-});
 
 module.exports = router;
