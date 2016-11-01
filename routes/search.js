fc05f639a2cb272a46c52f9ad225f73d58420695
Sang Min Na <xsangmin@gmail.com>
2016-11-01 04:11:35 -0400

    style search results and fix redirect routes

--- routes/search.js
+++ routes/search.js
@@ -1,14 +1,16 @@
-const router  = require('express').Router();
-const yelp    = require('../services/yelp');
+const router              = require('express').Router();
+const yelp                = require('../services/yelp');
+const { authenticate }    = require('../lib/auth');
 
 
-router.post('/', yelp.search, yelp.getLatLng, (req, res) => {
+router.post('/', authenticate, yelp.search, yelp.getLatLng, (req, res) => {
   // console.log('results *** ', res.yelpResults.businesses);
   res.render('google/map', {
     results               : res.yelpResults.businesses || [],
     fav                   : res.favorites || [],
     GOOGLE_MAP_API_KEY    : process.env.GOOGLE_MAP_API_KEY,
     latlngArray           : res.latlng || [],
+    user                  : res.user,
   });
 });
 
